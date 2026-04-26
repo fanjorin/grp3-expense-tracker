0ca0359d8aec25195b2e90cf485b26c073245de4
Byoc-3 <pogba.junior987@gmail.com>
2026-04-26 02:25:34 +0100

    feat: add budget banner component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import BudgetBanner from "./components/BudgetBanner";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
       <Navbar />
+      <BudgetBanner />
     </main>
   );
 }
