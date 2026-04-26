cb913735efb906d969481cf3a6cbb416c2097c10
Byoc-3 <pogba.junior987@gmail.com>
2026-04-26 02:33:42 +0100

    feat: add action bar component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import BudgetBanner from "./components/BudgetBanner";
+import ActionBar from "./components/ActionBar";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
       <Navbar />
       <BudgetBanner />
+      <ActionBar />
     </main>
   );
 }
