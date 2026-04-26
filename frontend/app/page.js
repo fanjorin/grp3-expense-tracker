5d47b6c09c883ab9a2d923f5ef1948b0eae22263
Byoc-3 <pogba.junior987@gmail.com>
2026-04-26 02:40:37 +0100

    feat: add charts and insights components

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import BudgetBanner from "./components/BudgetBanner";
 import ActionBar from "./components/ActionBar";
+import Charts from "./components/Charts";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <BudgetBanner />
       <ActionBar />
+      <Charts />
     </main>
   );
 }
