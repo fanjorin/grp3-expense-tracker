71ffd52370a99b342c4b66b9c292ff43fabcfeff
Byoc-3 <pogba.junior987@gmail.com>
2026-05-03 13:04:14 +0100

    feat: add learning page navbar

---
+++ frontend/app/landing/page.jsx
@@ -0,0 +1,9 @@
+import LandingNavbar from "./components/LandingNavbar";
+
+export default function LandingPage() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100">
+      <LandingNavbar />
+    </main>
+  );
+}
