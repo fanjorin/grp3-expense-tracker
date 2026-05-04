6018b0a9ae1d01c98f816e973ecfb6f5110f0780
okeke kelvin chigozie <chigozie.okeke@pau.edu.ng>
2026-05-05 00:30:17 +0100

    feat: add CORS to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import cors from "cors";    
 import routes from "./routes";
 
 const app: Express = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(morgan("tiny"));
 
