abe9a701ce93b0e73ba2785e829aed93c1e5bde2
Byoc-3 <pogba.junior987@gmail.com>
2026-05-18 10:15:05 +0100

    feat: add expense summary panel and complete add expense page

--- frontend/app/add-expense/page.jsx
+++ frontend/app/add-expense/page.jsx
@@ -2,6 +2,7 @@ import Sidebar from "./components/Sidebar";
 import AddExpenseHeader from "./components/AddExpenseHeader";
 import ExpenseForm from "./components/ExpenseForm";
 import ReceiptUpload from "./components/ReceiptUpload";
+import ExpenseSummary from "./components/ExpenseSummary";
 
 export default function AddExpensePage() {
   return (
@@ -25,6 +26,7 @@ export default function AddExpensePage() {
           {/* Right - Receipt + Summary */}
           <div className="col-span-1 flex flex-col gap-4">
             <ReceiptUpload />
+            <ExpenseSummary />
           </div>
 
         </div>
