2cb62d865f93ff472002ff1dd0b1bf1139747177
Tobani Esan-George <tobanigeorge@gmail.com>
2026-06-02 13:40:54 +0100

    fix: Update Poppins font configuration in layout

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,8 +2,10 @@ import { Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
