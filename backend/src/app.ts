86a4739f7412f0d6d2cd5ee32f9667a512abe3a8
Tobani Esan-George <tobanigeorge@gmail.com>
2026-06-02 12:47:27 +0100

    fix: update cors config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,7 @@ import * as _ from "./types/express";
 
 const app: Express = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: ["https://pennywiseaipro.vercel.app", "http://localhost:3000"] }));
 app.use(express.json());
 app.use(morgan("tiny"));
 
